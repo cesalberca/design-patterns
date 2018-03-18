2f24769d1ad224c75501c309ab10767832daf322
César Alberca <cesalberca@gmail.com>
2018-03-18 15:07:59 +0100

    Add singleton

---
+++ src/singleton/Main.kt
@@ -0,0 +1,8 @@
+package singleton
+
+fun main(args: Array<String>) {
+    Singleton
+    Singleton
+    Singleton
+    println(Singleton.state)
+}
---
+++ src/singleton/Singleton.kt
@@ -0,0 +1,11 @@
+package singleton
+
+object Singleton {
+
+    var state = 0
+
+    init {
+        state += 1
+    }
+
+}
